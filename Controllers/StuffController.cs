245e525b11fbc5b8de9b8de3394f38d1a7258fa2
xin-LianK <350824110@qq.com>
2018-03-16 14:28:10 +0800

    '--'

---
+++ Controllers/StuffController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiDemo.Controllers
+{
+    [Authorize]
+    public class StuffController : ApiController
+    {
+        Models.ApplicationDbContext db = new Models.ApplicationDbContext();
+        public StuffController()
+        {
+
+        }
+        public IHttpActionResult GetStuff(int Id)
+        {
+          var stuff=  db.Stuffs.FirstOrDefault(p => p.Id == Id);
+            if (stuff == null)
+                return NotFound();
+            return Ok(stuff);
+        }
+        public IHttpActionResult GetStuffs()
+        {
+            return Ok(db.Stuffs);
+        }
+        public IHttpActionResult AddStuff(Models.Stuff stuff)
+        {
+            db.Stuffs.Add(stuff);
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
